4945ced2ee37cef85149dd4e93d6d44ea9480401
Roopin <bhadriroopin748@gmail.com>
2020-04-06 03:25:22 +0530

    Updates

---
+++ precache-manifest.ba47d22f9522305924bf0835a5f5a559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46f152802237e8b19a5add444975d9e",
+    "url": "/React-todolist/index.html"
+  },
+  {
+    "revision": "8fb8428c12381a50fac2",
+    "url": "/React-todolist/static/css/main.a53395a6.chunk.css"
+  },
+  {
+    "revision": "1c8390861ac8fc8b3cd0",
+    "url": "/React-todolist/static/js/2.b59be8e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todolist/static/js/2.b59be8e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb8428c12381a50fac2",
+    "url": "/React-todolist/static/js/main.4c24bbdd.chunk.js"
+  },
+  {
+    "revision": "4d29858b9941e980169e",
+    "url": "/React-todolist/static/js/runtime-main.00137c64.js"
+  }
+]);
